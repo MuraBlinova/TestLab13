e7fd2b0abd2ba5738c1adefee32f43b9f499c655
MuraBlinova <99964993+MuraBlinova@users.noreply.github.com>
2024-04-14 20:06:10 +0300

    правка main

--- main.py
+++
@@ -1,8 +0,0 @@
-from selenium import webdriver
-
-def start_work():
-    driver = webdriver.Firefox()
-    print(driver)
-    adress = "https://ru.spbtv.com"
-    driver.get(adress)
-    driver.implicitly_wait(5)  # seconds
